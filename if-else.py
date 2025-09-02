c0a2be92e0cc7397d558622c301d8bfeeae52ff0
TruptiCoder <truptibalbudhe05@gmail.com>
2025-09-02 22:04:07 +0530

    basics

---
+++ if-else.py
@@ -0,0 +1,20 @@
+today_is_opposite_day = True
+
+# Set say_it_is_opposite_day based pn today_is_opposite_day:
+
+if today_is_opposite_day == True:
+    say_it_is_opposite_day = True
+else:
+    say_it_is_opposite_day = False
+
+# If it is opposite day, toggle say_it_is_opposite_day:
+
+if today_is_opposite_day == True:
+    say_it_is_opposite_day = not say_it_is_opposite_day
+
+# Say what day it is
+
+if say_it_is_opposite_day == True:
+    print("Today is Opposite day")
+else:
+    print("Today is not opposite day")
